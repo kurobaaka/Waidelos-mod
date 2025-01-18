e9068756bbd52a9a3fa602d7f27a92e67e708c61
Waideloss <psykhokidzxc@gmail.com>
2025-01-18 20:11:01 +0700

    12

--- src/main/java/net/waimod/items/ModItems.java
+++ src/main/java/net/waimod/items/ModItems.java
@@ -3,7 +3,9 @@ package net.waimod.items;
 import net.waimod.WaidelosMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -21,5 +23,7 @@ public class ModItems {
 
     public static void registredModItems() {
         WaidelosMod.LOGGER.info("registering mod items for" + WaidelosMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsIngridientTabItemGroup);
     }
 }
