3ca38f26aa6626f0158de22971f630e723b1f028
kurobaaka <psykhokidzxc@gmail.com>
2025-01-25 13:43:02 +0700

    hueta

--- src/main/java/net/infugogr/barracuda/entity/client/PirateRenderer.java
+++ src/main/java/net/infugogr/barracuda/entity/client/PirateRenderer.java
@@ -1,34 +1,34 @@
-package net.infugogr.barracuda.entity.client;
+// package net.infugogr.barracuda.entity.client;
 
-import net.infugogr.barracuda.Barracuda;
-import net.infugogr.barracuda.entity.custom.PirateEntity;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.MobEntityRenderer;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.Identifier;
+// import net.infugogr.barracuda.Barracuda;
+// import net.infugogr.barracuda.entity.custom.PirateEntity;
+// import net.minecraft.client.render.VertexConsumerProvider;
+// import net.minecraft.client.render.entity.EntityRendererFactory;
+// import net.minecraft.client.render.entity.MobEntityRenderer;
+// import net.minecraft.client.util.math.MatrixStack;
+// import net.minecraft.util.Identifier;
 
-public class PirateRenderer extends MobEntityRenderer<PirateEntity, PirateModel<PirateEntity>> {
-    private static final Identifier TEXTURE = new Identifier(Barracuda.MOD_ID, "textures/entity/porcupine.png");
+// public class PirateRenderer extends MobEntityRenderer<PirateEntity, PirateModel<PirateEntity>> {
+//     private static final Identifier TEXTURE = new Identifier(Barracuda.MOD_ID, "textures/entity/pirate.png");
 
-    public PirateRenderer(EntityRendererFactory.Context context) {
-        super(context, new PirateModel<>(context.getPart(ModModelLayers.PIRATE)), 0.6f);
-    }
+//     public PirateRenderer(EntityRendererFactory.Context context) {
+//         super(context, new PirateMФodel<>(context.getPart(ModModelLayers.PIRATE)), 0.6f);
+//     }
 
-    @Override
-    public Identifier getTexture(PirateEntity entity) {
-        return TEXTURE;
-    }
+//     @Override
+//     public Identifier getTexture(PirateEntity entity) {
+//         return TEXTURE;
+//     }
 
-    @Override
-    public void render(PirateEntity mobEntity, float f, float g, MatrixStack matrixStack,
-                       VertexConsumerProvider vertexConsumerProvider, int i) {
-        if(mobEntity.isBaby()) {
-            matrixStack.scale(0.5f, 0.5f, 0.5f);
-        } else {
-            matrixStack.scale(1f, 1f, 1f);
-        }
+//     @Override
+//     public void render(PirateEntity mobEntity, float f, float g, MatrixStack matrixStack,
+//                        VertexConsumerProvider vertexConsumerProvider, int i) {
+//         if(mobEntity.isBaby()) {
+//             matrixStack.scale(0.5f, 0.5f, 0.5f);
+//         } else {
+//             matrixStack.scale(1f, 1f, 1f);
+//         }
 
-        super.render(mobEntity, f, g, matrixStack, vertexConsumerProvider, i);
-    }
-}
+//         super.render(mobEntity, f, g, matrixStack, vertexConsumerProvider, i);
+//     }
+// }
